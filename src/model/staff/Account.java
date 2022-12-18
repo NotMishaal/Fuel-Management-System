4d66de4f8e9761332364eeed587294d9b667335f
Inuka Rodrigo <inuka.rodrigo@gmail.com>
2022-12-18 15:52:49 +0530

    Added the implementation of some functions under each class and added arrayLists and relevant objects according to the relationships between classes

--- src/model/staff/Account.java
+++ src/model/staff/Account.java
@@ -1,4 +1,52 @@
 package model.staff;
 
+import model.customer.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Account {
+    private String accountID;
+    private HashMap<Customer,Double> payment;
+    private ArrayList<Customer> customersServed;
+    private Accountant accountant;
+
+    public Account(String accountID, HashMap<Customer, Double> payment, ArrayList<Customer> customersServed, Accountant accountant) {
+        this.accountID = accountID;
+        this.payment = payment;
+        this.customersServed = customersServed;
+        this.accountant = accountant;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public HashMap<Customer, Double> getPayment() {
+        return payment;
+    }
+
+    public void setPayment(HashMap<Customer, Double> payment) {
+        this.payment = payment;
+    }
+
+    public ArrayList<Customer> getCustomersServed() {
+        return customersServed;
+    }
+
+    public void setCustomersServed(ArrayList<Customer> customersServed) {
+        this.customersServed = customersServed;
+    }
+
+    public Accountant getAccountant() {
+        return accountant;
+    }
+
+    public void setAccountant(Accountant accountant) {
+        this.accountant = accountant;
+    }
 }
--- src/model/staff/Accountant.java
+++ src/model/staff/Accountant.java
@@ -1,4 +1,34 @@
 package model.staff;
 
-public class Accountant {
+import java.util.ArrayList;
+
+public class Accountant extends Staff{
+    private ArrayList<Account> listOfAccounts;
+
+    public Accountant(String staffID, String name, ArrayList<Account> listOfAccounts) {
+        super(staffID, name);
+        this.listOfAccounts = listOfAccounts;
+    }
+
+    public boolean removeAccount(Account account){
+        int indexOfTheAccount = listOfAccounts.indexOf(account); // This is to check if the object parsed exist in the list
+        if (indexOfTheAccount == -1){
+            //If the object does not exist in the list
+            return false;
+        }else {
+            listOfAccounts.remove(indexOfTheAccount);
+            return true;
+        }
+    }
+
+    public boolean addAccount(Account account){
+        int indexOfTheAccount = listOfAccounts.indexOf(account);
+        if (indexOfTheAccount == -1){
+            //If the object does not exist in the list
+            this.listOfAccounts.add(account);
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
