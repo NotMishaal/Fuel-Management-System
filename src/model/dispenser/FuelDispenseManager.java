c1735cec95bc918a770cea7673708454927f072e
Yohan Abhishek <100549603+yohanAbhishek@users.noreply.github.com>
2022-12-28 09:21:01 +0530

    Updated fuel dispense manager

--- src/model/dispenser/FuelDispenseManager.java
+++ src/model/dispenser/FuelDispenseManager.java
@@ -3,8 +3,6 @@ package model.dispenser;
 import model.customer.Customer;
 
 public interface FuelDispenseManager {
-    public void getStats();
-
     public void dispenseFuel(double quantity);
 
     public void suspendDispenser();
