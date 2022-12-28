c0be03485d56c904f6de3e1f60caa2d2588d24d4
Inuka Rodrigo <inuka.rodrigo@gmail.com>
2022-12-28 07:30:05 +0530

    Removed the template functions I added temporary in Customer and FuelDispenseManager class

--- src/model/customer/Customer.java
+++ src/model/customer/Customer.java
@@ -1,11 +1,4 @@
 package model.customer;
 
 public class Customer {
-    public String getName(){
-        return "aaa";
-    }
-
-    public double getPumpedFuelAmount(){
-        return 0;
-    }
 }
--- src/model/dispenser/FuelDispenseManager.java
+++ src/model/dispenser/FuelDispenseManager.java
@@ -3,5 +3,4 @@ package model.dispenser;
 import model.customer.Customer;
 
 public interface FuelDispenseManager {
-    public void dispenseFuel(Customer customer, double quantity);
 }
