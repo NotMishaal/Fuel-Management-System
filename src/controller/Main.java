3f0d7fee159dd5a166b669e3ccb85abb704f44ba
Mishaal <mishaal.20210822@iit.ac.lk>
2022-12-28 20:35:38 +0530

    Finished queue implementation + added view/join queue logic to main method

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -16,9 +16,9 @@ public class Main {
         Queue petrolQueue2 = new Queue("Petrol", 2);
         Queue petrolQueue3 = new Queue("Petrol", 3);
         Queue petrolQueue4 = new Queue("Petrol", 4);
-        Queue dieselQueue1 = new Queue("Diesel", 1);
-        Queue dieselQueue2 = new Queue("Diesel", 2);
-        Queue dieselQueue3 = new Queue("Diesel", 3);
+        Queue dieselQueue1 = new Queue("Diesel", 5);
+        Queue dieselQueue2 = new Queue("Diesel", 6);
+        Queue dieselQueue3 = new Queue("Diesel", 7);
 
 
         // user input
@@ -30,7 +30,8 @@ public class Main {
                         while (true) {
                             switch (customerMenu()){
                                 case 1: // view queues
-                                    if (customer.getFuelType() == 0){
+                                    System.out.println("-Queues Available For You-");
+                                    if (customer.getFuelType() == 0){ // petrol queues
                                         if (customer.getVehicleType() == 1){
                                             petrolQueue1.display();
                                             petrolQueue2.display();
@@ -41,11 +42,53 @@ public class Main {
                                         } else {
                                             petrolQueue2.display();
                                         }
+                                    } else { // diesel queues
+                                        if (customer.getVehicleType() == 4){
+                                            dieselQueue1.display();
+                                        } else {
+                                            dieselQueue2.display();
+                                            dieselQueue3.display();
+                                        }
                                     }
                                     break;
 
                                 case 2: // join queue
+                                    System.out.println("-Assigning to Best Available Queue-");
+                                    if (customer.getFuelType() == 0) { // petrol vehicles
+                                        if (customer.getVehicleType() == 1){
+                                            if (!petrolQueue1.enqueue(customer)){ // try to add customer to queue 1
+                                                if (!petrolQueue2.enqueue(customer)){ // try to add customer to queue 2
+                                                    //TODO: add to common waiting queue
+                                                }
+                                            }
+                                        } else if (customer.getVehicleType() == 2){
+                                            if (!petrolQueue3.enqueue(customer)){
+                                                //TODO: add to common waiting queue
+                                            }
+                                        } else if (customer.getVehicleType() == 3){
+                                            if (!petrolQueue4.enqueue(customer)){
+                                                //TODO: add to common waiting queue
+                                            }
+                                        } else {
+                                            if (!petrolQueue2.enqueue(customer)){
+                                                //TODO: add to common waiting queue
+                                            }
+                                        }
+                                    } else {
+                                        if (customer.getVehicleType() == 4){
+                                            if (!dieselQueue1.enqueue(customer)){
+                                                //TODO: add to common waiting queue
+                                            } else {
+                                                if (!dieselQueue2.enqueue(customer)){
+                                                    if (!dieselQueue3.enqueue(customer)){
+                                                        //TODO: add to common waiting queue
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                     break;
+                                    
                                 case 3: // quit
                                     System.exit(0);
                                     break;
